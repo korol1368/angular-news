b90f2ba12d7b14a7d860ef8afb15f619fc0fcb49
Aleksandr Korol <korol1368@gmail.com>
2021-05-21 14:23:20 +0300

    Implements news-list and news-item components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {NewsListComponent} from './components/news/news-list/news-list.component';
+import {NewsItemComponent} from './components/news/news-item/news-item.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NewsListComponent, NewsItemComponent],
   imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
